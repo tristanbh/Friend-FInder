75e239a28fc636ab85697af0c9c16076ad5cdc3a
Tristan Benedict-Hall <tbenedicthall@Tristans-MacBook-Pro.local>
2018-02-14 14:57:05 -0500

    removed a bodyparser initialization function as it was used in an older version of the package and is now no longer needed.

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -1,10 +1,12 @@
+//file path for html
 var path = require('path');
 
 module.exports = function(app){
+	//get requests when user visits a page
 	app.get('/survey', function(req, res){
 		res.sendFile(path.join(__dirname + '/../public/survey.html'));
 	});
-
+	//default if patch isnt found
 	app.use(function(req, res){
 		res.sendFile(path.join(__dirname + '/../public/home.html'));
 	});
--- server.js
+++ server.js
@@ -1,39 +1,25 @@
+//npm packages
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+//server infor for express
 var app = express();
 var PORT = process.env.PORT || 3000;
 // app.use(express.static(__dirname + '/public'));
 
+//bodyparser 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-
+//routing infor for server
 require('./routing/api-routes.js')(app);
 require('./routing/html-routes.js')(app);
 
+//starts server
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
-
-// app.get("/", function(req, res) {
-// 	res.sendFile(path.join(__dirname, "./public/home.html"));
-// })
-
-// app.get("/survey.html", function(req, res) {
-// 	res.sendFile(path.join(__dirname, "./public/survey.html"));
-// })
-
-// app.get("/api/survey", function(req, res) {
-// 	return res.json(survey)
-// })
-
-// app.post('/api/survey'), function (req, res) {
-// 	let newSurvey = req.body;
-// 	console.log(newSurvey);
-// 	survey.push(newSurvey);
-// 	res.json(newSurvey);
-// };
